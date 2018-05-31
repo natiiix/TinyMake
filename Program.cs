970475e65972181a8b334666b4a9cd6ae1e4de44
Ivo Meixner <ivo.meixner@se7en.cz>
2018-05-31 04:43:44 +0200

    Write core code for reading the Makefile

--- Program.cs
+++ Program.cs
@@ -5,9 +5,30 @@ namespace TinyMake
 {
     public class Program
     {
+        private const string DEFAULT_MAKEFILE = "Makefile";
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Get the current directory path
+            string currentPath = Directory.GetCurrentDirectory();
+            // Get the path of the Makefile
+            string makefilePath = Path.Combine(currentPath, DEFAULT_MAKEFILE);
+
+            if (!File.Exists(makefilePath))
+            {
+                ExitWithError($"No Makefile in current directory ({currentPath})");
+            }
+
+            // Read the whole Makefile into a string
+            string makefile = File.ReadAllText(makefilePath);
+
+            // TODO: Process the Makefile
+        }
+
+        public static void ExitWithError(string errorMsg)
+        {
+            Console.Error.WriteLine("ERROR: " + errorMsg);
+            Environment.Exit(-1);
         }
     }
 }
